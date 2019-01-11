1472dbe1c51f4c35820bd0e75129e4a72950367b
AdamAtri <adam@atricoware.com>
2019-01-10 23:12:42 -0500

    notes for hedonism

---
+++ week1/hedonism.js
@@ -0,0 +1,31 @@
+module.exports.title = 'Hedonism - John Stuart Mill';
+module.exports.tags = ['ethics', 'utilitarianism'];
+
+module.exports.notes = [
+  'Utility (Utilitarianism), is also known as the Greatest Happiness Principle',
+  'actions are right in proportion to the happiness they produce, wrong as they tend to produce the opposite',
+  'happiness = pleasure / the absence of pain',
+  'Human beings have faculties more elevated than the animal appetites',
+  'It is quite compatible with the principle <not theory> of utility to recognize the fact that some kinds of pleasure are more desirable and more valuable than others.',
+  'Of two pleasures, if there be one that all or almost all who have experience of both give a decided preference, irrespective of any feeling of moral obligation to prefer it, that is the more desirable pleasure.',
+  'Those capable of appreciating and enjoying both give marked preference to the manner of existence which emplys their higher faculties.',
+  'No one would conscent to being lowered to obstensively beastly intelligence in order to derive greater physical pleasure from some action, unless truly desperate and unhappy.',
+  'A being whose capacities of enjoyment are low has the greatest chance of having them fully satisfied',
+  'It is better to be a human being dissatisfied, than a pig satisfied',
+  'infirmity of character often makes one elect for the nearer good, though it be the less valuable, no less between two bodily pleasures than between bodily and mental.',
+  'Capacity for the nobler feelings is in most natures a very tender plant, easily killed, not only by hostile influences, but by mere want of sustenance.',
+  
+
+];
+
+module.exports.observations = [
+  'Mill definitely believes in classes. p259c2 "A being of higher faculties requires more to make him happy...he can never really wish to sink into what he feels to be a lower grade of existence."',
+  '"Society" should be expected to stimulate the nobler feelings and exercise the higher capacity of people (p260 c1). Could propaganda that stimulated these nobler feelings be considered utilitarian?',
+  'Those who have gone before you are probably in a better position to inform your judgement.',
+],
+
+module.exports.questions = [
+  'What are Epicureans?',
+  'p260 last paragraph, "the judgement of those qualified by knowledge <expect this means the experiencial kind> of both, or that of the majority, must be admitted as final." has a weird authoritarian ring to it.',
+  
+];
